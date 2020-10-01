e223745593eb3c28e1d8d22acad9ac41a0d36e20
Janos Pollak <pollak.janos0@gmail.com>
2020-10-01 11:27:57 -0700

    Start frame reading, add TODOS, changed wording to make some code more clear

--- main.py
+++ main.py
@@ -8,30 +8,47 @@ def main():
     background_subtract()
 
 
+# def frame_processing():
+#     cap = cv.VideoCapture('./data_output/Background_Sub.avi')
+#     if not cap.isOpened():
+#         print('Error Opening Background_Sub video')
+#         sys.exit(-1)
+#     while True:
+#         (exists_frame, frame)  = cap.read()
+#         if not exists frame:
+#             break
+#         cv.imread()
+
+
 # Simple background subtraction
 # Saves video to output file
+# TODO: allow user to set --algo to switch between MOG2 and KNN
+# TODO: allow user to select output location, then MUST CONFIGURE frame processing() input
 def background_subtract():
-    back_sub = cv.createBackgroundSubtractorMOG2()
+    back_sub = cv.createBackgroundSubtractorMOG2(varThreshold=30, detectShadows=False)
     cap = cv.VideoCapture(file_and_path())
     if not cap.isOpened():
         print('Error opening video file')
         sys.exit(-1)
     # Obtains default resolution of frame & converts from float to int
-    frame_width = int(cap.get(3))
-    frame_height = int(cap.get(4))
-    print(int(cap.get(4)))
+    frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    print(frame_height)
+    print(frame_width)
     # Define the codec, create VideoWriter object.
     output = cv.VideoWriter('./data_output/Background_Sub.avi', cv.VideoWriter_fourcc('M', 'J', 'P', 'G'),
-                            10, (frame_width,frame_height), 0)
+                            10, (frame_width, frame_height), 0)
     while True:
         (exists_frame, frame) = cap.read()
 
         if not exists_frame:
             break
         fg_mask = back_sub.apply(frame)
+        # Puts the frame count on the original video
         cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
         cv.putText(frame, str(cap.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
                    cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
         output.write(fg_mask)
         cv.imshow('Frame', frame)
         cv.imshow('FG Mask', fg_mask)
@@ -40,6 +57,7 @@ def background_subtract():
     cap.release()
     output.release()
     cv.destroyAllWindows()
+
 
 # for now just selects first frame NOTE: BADLY WRITTEN
 def select_first_frame():
@@ -71,7 +89,6 @@ def crop_to_tube():
 
     cv.namedWindow('Frame', cv.WINDOW_AUTOSIZE)
 
-
     # Read until end of video
     while cap.isOpened():
         (exists_frame, frame) = cap.read()
@@ -90,14 +107,14 @@ def crop_to_tube():
     print("Success...?")
 
     # Define codec
+
+
 #    out = cv.VideoWriter('')
-
-
-
 
 
 # Prompts user to input file name
 # Returns path and name in directory
+# TODO: configure argparse so user selects file location, and other params
 def file_and_path():  # Will likely use argparse here
     input_file_name = "Sequence 01.avi"
     path = "./video_input/"
