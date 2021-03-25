43d520609a807b307885fba1fa18fa7e578018e4
Janos Pollak <pollak.janos0@gmail.com>
2021-03-25 23:16:59 +0800

    still working on getting the single solution polynomial/line interface to work. Need to make the function less confusing (inverted, not inverted???)

--- test.py
+++ test.py
@@ -1,47 +1,73 @@
 import numpy as np
 from scipy import optimize
+import math
 import matplotlib.pyplot as plt
 import cv2 as cv
 
 
 # https://gist.github.com/ruoyu0088/70effade57483355bbd18b31dc370f2a
 
-def main(tongue_points, meniscus, i):
-    # tongue_points = np.load('./data_output/tongue_points.npy', allow_pickle=True)
-    print(np.arange(np.shape(tongue_points)[0]))
-    meniscus = meniscus[i]
+def main():
+    meniscus_eq = np.asarray([-1.76432786e-02, 1.23399985e+00, 4.36404085e+01])
+    points = np.load('./data_output/mog_bg_sub.npy')
+    meniscus = np.load('./data_output/meniscus_points.npy')
+    # points = np.load('./data_output/tongue_points.npy', allow_pickle=True)
+
+    meniscus = meniscus[447]
     meniscus_max = np.max(meniscus)
-    tongue_points = tongue_points[i]
+    points = points[447]
 
-    cv.imshow('frame', tongue_points)
-    tongue_points = np.asarray(tongue_points)
-    print(np.shape(tongue_points))
+    cv.imshow('frame', points)
+    points = np.asarray(points)
+    print(np.shape(points))
 
-    tongue_points[:, :meniscus_max + 2] = 0  # setting lower bounds (+ 2 pixels to remove artifacts due to the meniscus)
-    tongue_points[:, -1:] = 0  # setting upper bounds
-    cv.imshow('frame2', tongue_points)
-    y, x = tongue_points.nonzero()
+    points[:, :meniscus_max + 2] = 0  # setting lower bounds (+ 2 pixels to remove artifacts due to the meniscus)
+    points[:, 163:] = 0  # setting upper bounds
+    cv.imshow('frame2', points)
+    y, x = points.nonzero()
 
-    # x = np.arange(len(tongue_points))
-    # y = tongue_points
+    # x = np.arange(len(points))
+    # y = points
     # x = x[40:]
-    # y = tongue_points[40:]
+    # y = points[40:]
     # print(y)
 
     px, py = segments_fit(x, y)
     print('x: ', px)
     print('y: ', py)
 
+    solx, soly = intercepts(px, py, meniscus_eq)
+    print(solx, soly)
+
     plt.plot(x, y, 'o')
     plt.plot(px, py, 'or-')
+    plt.plot([solx], [soly], 'rx')
     plt.show()
 
 
-def intercepts(x1, x2, y1, y2):
-    m = (y1 - y2) / (x1 - x2)
-    b = -x1 * m + y1
+def intercepts(x, y, meniscus):
+    if y[0] - y[1] == 0:  # Then 1 intercept
+        soly = y
+        # solx =  I STOPPED WORKING HERE
 
-    # x = ((-B + m + - Sqrt[4 A b + B ^ 2 - 4 A C - 2 B m + m ^ 2]) / (2 A))
+    m = (x[0] - x[1]) / (y[0] - y[1])  # Reversed so the line is inverted
+    intercept = -y[0] * m + x[0]  # Reversed so the line is inverted
+    print(m, intercept)
+
+    a = meniscus[0]
+    b = meniscus[1] - m
+    c = meniscus[2] - intercept
+
+    # calculate the discriminant
+    d = (b ** 2) - (4 * a * c)
+
+    # find two solutions
+    sol1 = (-b - math.sqrt(d)) / (2 * a)
+    sol2 = (-b + math.sqrt(d)) / (2 * a)
+    print(sol1)
+    soly = m * sol1 + intercept
+
+    return soly, sol1  # Reversed so the number is re-inverted
 
 
 # Returns two separate x and y variables containing the coordinates
@@ -73,8 +99,4 @@ def segments_fit(X, Y, count=2):
 
 
 if __name__ == '__main__':
-    tongue_points = np.load('./data_output/mog_bg_sub.npy')
-    meniscus = np.load('./data_output/meniscus_points.npy')
-    for i in np.arange(np.size(tongue_points[0])):
-        main(tongue_points, meniscus, i)
-
+    main()
