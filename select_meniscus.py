e4e13682d2e81c44c0ebd194a02c6d9009b8ec40
Janos Pollak <pollak.janos0@gmail.com>
2021-07-09 00:59:59 -0700

    added comments, minor code changes, removed unused code

--- select_meniscus.py
+++ select_meniscus.py
@@ -3,13 +3,15 @@ import cv2 as cv
 import sys
 
 
+# Returns the coordinates of the points where the meniscus intersects with the tongue.
+# The user selects the points from the inputted video (array of images)
+# The inputted video must be a numpy array and can be black and white or color
 def get_meniscus(selected_frames):
     # selected_frames = np.load('./data_output/selected_frames.npy')
 
     meniscus_coords = []
     frame_num = 0
     total_frames = np.shape(selected_frames)[0]
-
     print()
     print('===================')
     print('You will now select the point at which the tongue intersects with the meniscus')
@@ -22,7 +24,7 @@ def get_meniscus(selected_frames):
         print("Press 'SPACE' to continue, 'R' to redo, 'Q' to quit ")
         frame = selected_frames[frame_num, :, :]
         coords = select_intersection(frame)
-        if coords is not -1:
+        if coords != -1:
             meniscus_coords.append(coords)
             frame_num += 1
 
@@ -30,6 +32,12 @@ def get_meniscus(selected_frames):
     return meniscus_coords
 
 
+# Returns the coordinates of a user's first click. Also draws a circle under the clicked point
+# so the user can assess whether they want to redo or continue.
+# Takes an image as input, along with user mouse and keyboard input
+# If the user doesn't click, or wants to redo ('R'), the function returns -1
+# If the user presses Q or ESC, the program closes.
+# If the user clicks and presses SPACE, the coordinates are returned as tuples (x, y)
 def select_intersection(frame):
     class CoordinateStore:
         def __init__(self):
@@ -38,15 +46,16 @@ def select_intersection(frame):
 
         def select_point(self, event, x, y, flags, param):
             if event == cv.EVENT_LBUTTONDOWN and not self.clicked:
-                cv.circle(img, (x, y), 2, (0, 255, 0), -1)
+                cv.circle(img, (x, y), 3, (0, 120, 0), -1)
                 self.points = (x, y)
                 self.clicked = True
 
     # instantiate class
     coordinateStore1 = CoordinateStore()
 
-    # Create a black image, a window and bind the function to window
+    # Creates a copy of the image so the array isn't changed
     img = frame.copy()
+    img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
     cv.namedWindow('image')
     cv.setMouseCallback('image', coordinateStore1.select_point)
 
@@ -62,67 +71,6 @@ def select_intersection(frame):
         elif k == ord('r'):  # R
             cv.destroyAllWindows()
             return -1
-
-
-    print("Selected Coordinates: ")
-    # for i in coordinateStore1.points:
-    print(coordinateStore1.points)
-
-
-
-
-
-
-# # Handles mouse clicks
-# def click_and_crop(event, x, y, flags, param):
-#     if event == cv.EVENT_LBUTTONDOWN:
-#         click_coords = (x, y)
-#         MENISCUS_COORDS.append(click_coords)
-#         print(f'Clicked corner {len(MENISCUS_COORDS)}/4')
-
-
-# Displays image, prompts user to click 4 corners of desired object.
-# Returns the data which can later be used to zoom into the area contained
-# within the four points
-def select_corners(img):
-    global MENISCUS_COORDS  # double check that I actually need a global variable here
-    MENISCUS_COORDS = []
-
-    win_name = "Click the 4 Corners (Press 'Q' to Cancel)"
-    cv.namedWindow(win_name)
-    cv.setMouseCallback(win_name, click_and_crop)
-
-    # While less than 4 corners are clicked
-    while len(MENISCUS_COORDS) < 4:
-        cv.imshow(win_name, img)
-        key = cv.waitKey(1) & 0xFF
-        if key == 27 or key == ord("q"):  # If the user presses Q or ESC
-            cv.destroyAllWindows()
-            sys.exit(-1)
-    cv.destroyAllWindows()
-    corner_array = np.array(MENISCUS_COORDS, dtype="float32")
-    transform_data = get_four_point_transform(corner_array)
-    warped = apply_four_point_transform(img, transform_data)
-
-    print(f"The dimensions of the cropped image: Height={warped.shape[0]}, Width={warped.shape[1]}")
-    print()
-    print("If UNSATISFIED with the crop, press 'Q' to Cancel")
-    print("If SATISFIED with the crop, please select the direction of the opening of the tube to continue.")
-    print("'L' if the opening is on the left, and 'R' if the opening is on the right")
-    while True:
-        cv.imshow("Cropped image. Read console for instructions", warped)
-        key = cv.waitKey(0) & 0xFF
-        if key == 27 or key == ord("q"):  # If the user presses Q or ESC
-            cv.destroyAllWindows()
-            sys.exit(-1)
-        elif key == ord("l"):
-            opening_is_left = True
-            break
-        elif key == ord("r"):
-            opening_is_left = False
-            break
-    cv.destroyAllWindows()
-    return transform_data, opening_is_left
 
 
 if __name__ == '__main__':
