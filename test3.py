128d83a744cda4f24b9510486a7a7bf1bc1412f5
Janos Pollak <pollak.janos0@gmail.com>
2021-07-14 01:56:30 -0700

    now a video player that allows the user to rewind and go forward frame by frame, can be used to compare datasets

--- test3.py
+++ test3.py
@@ -2,20 +2,33 @@ import cv2 as cv
 import numpy as np
 import sys
 
-a = np.asarray([[0, 255],
-                [255, 0]], dtype=np.uint8)
+# a = np.asarray([[0, 255],
+#                 [255, 0]], dtype=np.uint8)
+a = np.load('./data_output/line_vid_color.npy')
+a2 = np.load('./data_output/line_vid.npy')
+# frame_num = 413
+# a2 = a2[frame_num]
+#
+# a = a[frame_num]
 print(a)
-x_dim, y_dim = np.asarray(np.shape(a))
+ignore, y_dim, x_dim, px_value = np.asarray(np.shape(a))
 magnification = 3
 dim = (x_dim * magnification, y_dim * magnification)
-a = cv.resize(a, dim, interpolation=cv.INTER_NEAREST)
-cv.imshow('frame', a)
+frame_num = 412
 while True:
-    cv.imshow('frame', a)
-    key = cv.waitKey(1) & 0xFF
+    print(frame_num)
+    frame = cv.resize(a[frame_num], dim, interpolation=cv.INTER_NEAREST)
+    frame2 = cv.resize(a2[frame_num], dim, interpolation=cv.INTER_NEAREST)
+    cv.imshow('frame', frame)
+    cv.imshow('frame2', frame2)
+    key = cv.waitKey(0) & 0xFF
     if key == 27 or key == ord("q"):  # If the user presses Q or ESC
         cv.destroyAllWindows()
         sys.exit(-1)
+    elif key == ord('j'):
+        frame_num -= 1
+    elif key == ord('l'):
+        frame_num += 1
 cv.destroyAllWindows()
 
 
