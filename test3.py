713047b4788d51e516318cfedc2e52d4390d4d14
Janos Pollak <pollak.janos0@gmail.com>
2021-07-15 18:22:33 -0700

    added magnification function

--- test3.py
+++ test3.py
@@ -13,11 +13,11 @@ a3 = np.load('./data_output/line_vid_tongue.npy')
 # a = a[frame_num]
 print(a)
 ignore, y_dim, x_dim, px_value = np.asarray(np.shape(a))
-magnification = 3
-dim = (x_dim * magnification, y_dim * magnification)
+magnification = 1
 frame_num = 412
 while True:
     print(frame_num)
+    dim = (x_dim * magnification, y_dim * magnification)
     frame = cv.resize(a[frame_num], dim, interpolation=cv.INTER_NEAREST)
     frame2 = cv.resize(a2[frame_num], dim, interpolation=cv.INTER_NEAREST)
     frame3 = cv.resize(a3[frame_num], dim, interpolation=cv.INTER_NEAREST)
@@ -32,6 +32,10 @@ while True:
         frame_num -= 1
     elif key == ord('l'):
         frame_num += 1
+    elif key in {ord('='), ord("+")}:
+        magnification += 1
+    elif (key == ord("-")) and magnification > 1:
+        magnification -= 1
 cv.destroyAllWindows()
 
 
