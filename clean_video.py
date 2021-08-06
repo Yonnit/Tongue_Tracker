45c6c3e5eea5769890efafa672bbddb00ff2d328
Janos Pollak <pollak.janos0@gmail.com>
2021-08-05 18:20:18 -0700

    implemented cleaning function (remove_distant_components)

--- clean_video.py
+++ clean_video.py
@@ -20,14 +20,51 @@ def main():
 
 
 # Takes a black and white video array.
-# Returns video array with isolated pixels removed.
+# Returns it cleaned of isolated pixels (random noise) and distant components (usually bubbles)
 def clean_bg_sub(video_array):
     cleaned = []
     for frame in video_array:
-        cleaned.append(remove_isolated_pixels(frame))
+        no_isolated = remove_isolated_pixels(frame)
+        cleaned_frame = remove_distant_components(no_isolated)
+        cleaned.append(cleaned_frame)
     return np.asarray(cleaned)
 
 
+def remove_distant_components(image):
+    # find all your connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(image, connectivity=8)
+    # connectedComponentswithStats yields every seperated component with information on each of them, such as size
+    # the following part is just taking out the background which is also considered a component,
+    # but most of the time we don't want that.
+    sizes = stats[1:, -1];
+    centroids = centroids[1:].astype(int)
+    # nb_components = nb_components - 1
+
+    # minimum size of particles that go un-vetted, and are unquestionably either the tongue or meniscus.
+    # They are kept and are what the dist_from_arr are based on.
+    min_size = np.mean(sizes)  # Might what to make it mean / 2
+
+    # your answer image
+    cleaned = np.zeros(output.shape, dtype=np.uint8)
+    # for every component in the image, you keep it only if it's above min_size
+    for i in range(0, nb_components):
+        if sizes[i] >= min_size:
+            cleaned[output == i + 1] = 255
+
+    # Shows the distance from the nearest component that met the min_size requirement
+    inverted = cv.bitwise_not(cleaned)
+    dist_from_arr = cv.distanceTransform(inverted, cv.DIST_C, cv.DIST_MASK_PRECISE)
+
+    # Puts components that are close enough to the tongue/meniscus back into the cleaned picture
+    for i, centroid in enumerate(centroids):
+        if dist_from_arr[centroid[1], centroid[0]] < 10:
+            cleaned[output == i + 1] = 255
+
+    return cleaned
+
+
+# Takes a binary image and removes the isolated pixels
+#           Could be merged with remove distant components but I'm too lazy
 def remove_isolated_pixels(image):
     connectivity = 8
 
