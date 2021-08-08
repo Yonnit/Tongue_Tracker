23052601c0eb33cec0d005162114d076ae28c4d9
Janos Pollak <pollak.janos0@gmail.com>
2021-08-07 21:46:31 -0700

    made the video player into a proper function. Now can play an arbitrary number of videos at the same time

--- multi_video_player.py
+++ multi_video_player.py
@@ -2,41 +2,44 @@ import cv2 as cv
 import numpy as np
 import sys
 
-# a = np.asarray([[0, 255],
-#                 [255, 0]], dtype=np.uint8)
-path = './data_output/B2-S20__20210805_224343'
-a = np.load(f'{path}/bw_line.npy')
-a2 = np.load(f'{path}/color_line.npy')
-a3 = np.load(f'{path}/only_tongue_line.npy')
-# frame_num = 413
-# a2 = a2[frame_num]
-#
-# a = a[frame_num]
-print(a)
-ignore, y_dim, x_dim, px_value = np.asarray(np.shape(a))
-magnification = 1
-frame_num = 682
-while True:
-    print(frame_num)
-    dim = (x_dim * magnification, y_dim * magnification)
-    frame = cv.resize(a[frame_num], dim, interpolation=cv.INTER_NEAREST)
-    frame2 = cv.resize(a2[frame_num], dim, interpolation=cv.INTER_NEAREST)
-    frame3 = cv.resize(a3[frame_num], dim, interpolation=cv.INTER_NEAREST)
-    cv.imshow('frame', frame)
-    cv.imshow('frame2', frame2)
-    cv.imshow('frame3', frame3)
-    key = cv.waitKey(0) & 0xFF
-    if key == 27 or key == ord("q"):  # If the user presses Q or ESC
-        cv.destroyAllWindows()
-        sys.exit(-1)
-    elif key == ord('j'):
-        frame_num -= 1
-    elif key == ord('l'):
-        frame_num += 1
-    elif key in {ord('='), ord("+")}:
-        magnification += 1
-    elif (key == ord("-")) and magnification > 1:
-        magnification -= 1
-cv.destroyAllWindows()
+
+def main():
+    path = './data_output/B2-S20__20210805_224343'
+    a = np.load(f'{path}/bw_line.npy')
+    a2 = np.load(f'{path}/color_line.npy')
+    a3 = np.load(f'{path}/only_tongue_line.npy')
+    video_player(682, a, a2, a3)
 
 
+def video_player(starting_frame, *args):
+    try:
+        total_frames, y_dim, x_dim, px_value = np.asarray(np.shape(args[1]))
+    except ValueError:
+        total_frames, y_dim, x_dim = np.asarray(np.shape(args[1]))  # For single channel images (black and white)
+
+    print(f'Starting on frame {starting_frame} of {total_frames}')
+    magnification = 1
+    frame_num = starting_frame
+    while True:
+        print(frame_num)
+        dim = (x_dim * magnification, y_dim * magnification)
+        for i, video in enumerate(args):
+            frame = cv.resize(video[frame_num], dim, interpolation=cv.INTER_NEAREST)
+            cv.imshow(f'video_{i}', frame)
+        key = cv.waitKey(0) & 0xFF
+        if key == 27 or key == ord("q"):  # If the user presses Q or ESC
+            break
+        elif key == ord('j'):
+            frame_num -= 1
+        elif key == ord('l'):
+            frame_num += 1
+        elif key in {ord('='), ord("+")}:
+            magnification += 1
+        elif (key == ord("-")) and magnification > 1:
+            magnification -= 1
+    cv.destroyAllWindows()
+    sys.exit(-1)
+
+
+if __name__ == '__main__':
+    main()
