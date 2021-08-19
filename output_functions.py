2ff17a45f0906f60713358057e23d12552cef6bf
Janos Pollak <pollak.janos0@gmail.com>
2021-08-18 18:33:28 -0700

    implementing saving csv of just the maxes

--- output_functions.py
+++ output_functions.py
@@ -7,7 +7,9 @@ import os
 def save_results(user_input, tongue_lengths, process_data):
     input_file_name = path_leaf(user_input['input']).split(".")[0]
     directory_path = make_directory(user_input['output'], input_file_name)
-
+    tf = tongue_lengths[:, 3]
+    tf = tf.astype(np.bool)
+    save_txt(tongue_lengths[tf], directory_path, input_file_name, 'just_maxes')
     save_txt(tongue_lengths, directory_path, input_file_name, 'tongue_lengths')
     save_process(directory_path, **process_data)
     print()
